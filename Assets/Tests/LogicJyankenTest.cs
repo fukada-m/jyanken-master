e2b2c96922ed603acafe9447406513c02d985a2c
fukada <fukada.m888@gmail.com>
2025-01-31 23:19:12 +0900

    じゃんけん全パターンを網羅

--- Assets/Tests/LogicJyankenTest.cs
+++ Assets/Tests/LogicJyankenTest.cs
@@ -9,6 +9,9 @@ public class LogicJyankenTest
 
     // あいこのテスト
     [TestCase(GameManager.Sign.Stone, GameManager.Sign.Stone)]
+    [TestCase(GameManager.Sign.Paper, GameManager.Sign.Paper)]
+    [TestCase(GameManager.Sign.Scissors, GameManager.Sign.Scissors)]
+
     public void LogicJyankenTestDraw(GameManager.Sign myself, GameManager.Sign enemy)
     {
         
@@ -19,6 +22,9 @@ public class LogicJyankenTest
 
     // 自分が勝つときのテスト
     [TestCase(GameManager.Sign.Stone, GameManager.Sign.Scissors)]
+    [TestCase(GameManager.Sign.Scissors, GameManager.Sign.Paper)]
+    [TestCase(GameManager.Sign.Paper, GameManager.Sign.Stone)]
+
     public void LogicJyankenTestWin(GameManager.Sign myself, GameManager.Sign enemy)
     {
         var result = LogicJyanken.Judge(myself, enemy);
@@ -28,6 +34,8 @@ public class LogicJyankenTest
 
     // 自分が負けるときのテスト
     [TestCase(GameManager.Sign.Stone, GameManager.Sign.Paper)]
+    [TestCase(GameManager.Sign.Scissors, GameManager.Sign.Stone)]
+    [TestCase(GameManager.Sign.Paper, GameManager.Sign.Scissors)]
     public void LogicJyankenTestLose(GameManager.Sign myself, GameManager.Sign enemy)
     {
         var result = LogicJyanken.Judge(myself, enemy);
