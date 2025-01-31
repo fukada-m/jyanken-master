416087c2e8f9fdc4b13b6a703d92a4ad156ea9e9
fukada <fukada.m888@gmail.com>
2025-01-31 23:11:14 +0900

    じゃんけんのロジックを追加

--- Assets/Scripts/LogicJyanken.cs
+++ Assets/Scripts/LogicJyanken.cs
@@ -4,6 +4,19 @@ public class LogicJyanken
 {
     public static string Judge(GameManager.Sign myself, GameManager.Sign enemy)
     {
-        return "draw";
+        var stone = GameManager.Sign.Stone;
+        var paper = GameManager.Sign.Paper;
+        var scissors = GameManager.Sign.Scissors;
+
+        // あいこのロジック
+        if (myself == enemy) return "draw";
+
+        //勝つときのロジック
+        if (myself == paper && enemy == stone) return "win";
+        if (myself == stone && enemy == scissors) return "win";
+        if (myself == scissors && enemy == paper) return "win";
+
+        //残りのパターンは負け
+        return "lose";
     } 
 }
