db7bf09230c79dab314d7c8fa458ed9854a8815c
fukada <fukada.m888@gmail.com>
2025-02-02 08:30:29 +0900

    HandButtonsの単体テストを追加

---
+++ Assets/Tests/UnitTest/HandButtonsTest.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Moq;
+
+public class HandButtonsTest
+{
+    HandButtons handButtons;
+    Mock<IHand> mockHand;
+
+    [SetUp]
+    public void HandButtonsTestSimplePasses()
+    {
+        var handButtonsObject = new GameObject("HandButtons");
+        handButtons = handButtonsObject.AddComponent<HandButtons>();
+        mockHand = new Mock<IHand>();
+        // Hand ‚ÌˆË‘¶ŠÖŒW‚ðÝ’è
+        typeof(HandButtons)
+            .GetField("hand", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+            .SetValue(handButtons, mockHand.Object);
+    }
+
+    [Test]
+    public void OnClickStoneButton_SetsHandToStone()
+    {
+        handButtons.onClickStoneButton();
+        mockHand.Verify(h => h.SetCurrent(GameManager.Sign.Stone), Times.Once);
+    }
+
+    [Test]
+    public void OnClickPaperButton_SetsHandToPaper()
+    {
+        handButtons.onClickPaperButton();
+        mockHand.Verify(h => h.SetCurrent(GameManager.Sign.Paper), Times.Once);
+    }
+
+    [Test]
+    public void OnClickScissorsButton_SetsHandToScissors()
+    {
+        handButtons.onClickScissorsButton();
+        mockHand.Verify(h => h.SetCurrent(GameManager.Sign.Scissors), Times.Once);
+    }
+
+}
