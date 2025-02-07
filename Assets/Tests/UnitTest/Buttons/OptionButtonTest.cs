2f620815780244ca14ddfef2b6432f35d4b1a025
fukada <fukada.m888@gmail.com>
2025-02-07 09:11:39 +0900

    テストで使うオブジェクトの定義まで行った

--- Assets/Tests/UnitTest/Buttons/OptionButtonTest.cs
+++ Assets/Tests/UnitTest/Buttons/OptionButtonTest.cs
@@ -5,20 +5,23 @@ using UnityEngine.TestTools;
 
 public class OptionButtonTest
 {
+    OptionButton optionButton;
+    GameObject setting;
+    GameObject menuButtons;
+
+    [SetUp]
+    public void OptionButtonSetUp()
+    {
+        optionButton = new GameObject("OptionButton").AddComponent<OptionButton>();
+        setting = new GameObject("Setting");
+        menuButtons = new GameObject("MenuButtons");
+    } 
     // A Test behaves as an ordinary method
     [Test]
-    public void OptionButtonTestSimplePasses()
+    public void OptionButton_ShowSetting()
     {
-        // Use the Assert class to test conditions
+
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator OptionButtonTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
+ 
 }
