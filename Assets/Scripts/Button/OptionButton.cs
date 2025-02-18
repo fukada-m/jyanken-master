ae7cebae17de3a1d1c97051896a461462a6b597a
fukada <fukada.m888@gmail.com>
2025-02-18 22:25:10 +0900

    オプションボタンのテスト

--- Assets/Scripts/Button/OptionButton.cs
+++ Assets/Scripts/Button/OptionButton.cs
@@ -6,10 +6,21 @@ public class OptionButton : MonoBehaviour
     GameObject settingModal;
     [SerializeField]
     GameObject menuButtons;
+    [SerializeField]
+    GameObject massageText;
+
+    // テスト用の依存関係を注入するメソッド
+    public void Initialize(GameObject s, GameObject m1, GameObject m2)
+    {
+        settingModal = s;
+        menuButtons = m1;
+        massageText = m2;
+    }
 
     public void OnClickButton()
     {
         settingModal.SetActive(true);
         menuButtons.SetActive(false);
+        massageText.SetActive(false);
     }
 }
