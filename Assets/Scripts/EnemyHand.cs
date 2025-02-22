6bca5d2a985cc9aa305e8d0d9e4d3b20199576e8
fukada <fukada.m888@gmail.com>
2025-02-23 00:26:48 +0900

    Signは使わない

--- Assets/Scripts/EnemyHand.cs
+++ Assets/Scripts/EnemyHand.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class EnemyHand : IEnemyHand
 {
-    ISign sign;
     Sign.Hand stone = Sign.Hand.Stone;
     Sign.Hand scissors = Sign.Hand.Scissors;
     Sign.Hand paper = Sign.Hand.Paper;
