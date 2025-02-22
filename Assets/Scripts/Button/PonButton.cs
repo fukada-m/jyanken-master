5ec2290f82a319e3a050e167570f7ef05728f0c9
fukada <fukada.m888@gmail.com>
2025-02-22 23:11:22 +0900

    ポンボタンを押したら相手がグーを選ぶとこまで作成

--- Assets/Scripts/Button/PonButton.cs
+++ Assets/Scripts/Button/PonButton.cs
@@ -4,35 +4,43 @@ public class PonButton : MonoBehaviour
 {
     [SerializeField]
     GameObject handButtons;
+    [SerializeField]
+    GameObject ponButton;
     IObserver messageText;
     Notify notify;
     ILogicJyanken logicJyanken;
     IEnemyHand enemyHand;
+    ISign sign;
 
     void Start()
     {
-        logicJyanken = new LogicJyanken(); 
-        enemyHand = new EnemyHand();
-        notify = new Notify();
         messageText = GameObject.Find("MessageText").GetComponent<IObserver>();
+        notify = new Notify();
         notify.AddObserver(messageText);
+        logicJyanken = new LogicJyanken();
+        enemyHand = new EnemyHand();
+        sign = new Sign();
     }
 
     // テスト用の依存関係を注入するメソッド
-    public void Initialize(GameObject h, IObserver m, Notify n, ILogicJyanken l, IEnemyHand e)
+    public void Initialize(GameObject h, GameObject p, IObserver m, Notify n, ILogicJyanken l, IEnemyHand e, ISign s)
     {
         handButtons = h;
+        ponButton = p;
         messageText = m;
         notify = n;
         logicJyanken = l;
         enemyHand = e;
+        sign = s;
     }
 
     public void OnClickButton()
     {
         handButtons.SetActive(false);
+        ponButton.SetActive(false);
         var enemyChoseHand = enemyHand.PickHand();
-        notify.SetTextNotify($"相手は{enemyChoseHand}を選びました");
+        var hand = sign.ConvertHandToJapanese(enemyChoseHand);
+        notify.SetTextNotify($"相手は{hand}を選びました");
         var result = logicJyanken.Judge(Sign.Hand.Scissors, enemyChoseHand);
     }
 
--- Assets/Tests/UnitTest/Button/PonButtonTest.cs
+++ Assets/Tests/UnitTest/Button/PonButtonTest.cs
@@ -7,23 +7,36 @@ using Moq;
 public class PonButtonTest
 {
     GameObject handButtons;
+    GameObject ponButtonOBJ;
     PonButton ponButton;
     Mock<IObserver> mockObserver;
     Mock<Notify> mockNotify;
     Mock<ILogicJyanken> mockLogicJyanken;
     Mock<IEnemyHand> mockEnemyHand;
+    Mock<ISign> mockSign;
 
     [SetUp]
     public void PonButtonSetUp()
     {
-        handButtons = new GameObject("handButtons");
+        handButtons = new GameObject("HandButtons");
+        ponButtonOBJ = new GameObject("PonButton");
         ponButton = new GameObject().AddComponent<PonButton>();
         mockObserver = new Mock<IObserver>();
         mockNotify = new Mock<Notify>();
         mockLogicJyanken = new Mock<ILogicJyanken>();
         mockEnemyHand = new Mock<IEnemyHand>();
+        mockSign = new Mock<ISign>();
         mockEnemyHand.Setup(s => s.PickHand()).Returns(Sign.Hand.Stone);
-        ponButton.Initialize(handButtons, mockObserver.Object,mockNotify.Object, mockLogicJyanken.Object, mockEnemyHand.Object);
+        mockSign.Setup(m => m.ConvertHandToJapanese(Sign.Hand.Stone)).Returns("グー");
+        ponButton.Initialize(
+            handButtons,
+            ponButtonOBJ,
+            mockObserver.Object,
+            mockNotify.Object,
+            mockLogicJyanken.Object,
+            mockEnemyHand.Object,
+            mockSign.Object
+         );
     }
     // A Test behaves as an ordinary method
     [Test]
@@ -32,9 +45,16 @@ public class PonButtonTest
         ponButton.OnClickButton();
         // ハンドボタンズは非表示になる
         Assert.IsFalse(handButtons.activeSelf);
+        // ポンボタンも非表示になる
+        Assert.IsFalse(ponButtonOBJ.activeSelf);
+        // CPUが出す手を決める
         mockEnemyHand.Verify(e => e.PickHand(), Times.Once);
-        // じゃんけんの判定処理が呼ばれたか確認
-         //mockLogicJyanken.Verify(l => l.Judge(Sign.Hand.Scissors, Sign.Hand.Stone), Times.Once);
+        // オブザーバーに通知を送る
+        mockNotify.Verify(m => m.SetTextNotify("相手はグーを選びました"), Times.Once);
+        // じゃんけんの勝敗判定処理が呼ばれる
+        mockLogicJyanken.Verify(l => l.Judge(Sign.Hand.Scissors, Sign.Hand.Stone), Times.Once);
+        // オブザーバーに通知を送る
+        mockNotify.Verify(m => m.SetTextNotify("あなたの負けです"), Times.Once);
     }
 
 }
