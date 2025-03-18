e6ec6101467d0ab53556bb82eda8a6e161cbc8f6
fukada <fukada.m888@gmail.com>
2025-03-18 21:41:37 +0900

    AgainButton作成

---
+++ Assets/Scripts/Button/AgainButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AgainButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnClickButton()
+    {
+
+    }
+}
